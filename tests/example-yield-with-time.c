9b881f5f495ff83b7f51fcf988a55872366c9613
leosun <leosunsl@outlook.com>
2023-02-01 15:16:57 +0800

    fix example

--- tests/example-yield-with-time.c
+++ tests/example-yield-with-time.c
@@ -1,40 +1,47 @@
-
 #include "pt.h"
+#include "pt-sem.h"
 #include "pt_ex_win32.h"
 
-#include <stdio.h> /* For printf(). */
+#include <stdio.h>
 
-static int protothread1_flag, protothread2_flag;
+static struct pt pt1, pt2;
+static struct pt_sem detect_evt;
 
 PT_INTERVAL_INIT();
 
-static int
-protothread1(struct pt *pt)
+static int main_thread(struct pt *pt)
 {
   PT_BEGIN(pt);
+
+  while (1) {
+    PT_SEM_WAIT(pt, &detect_evt);
+    printf("main thread: doing jobs\n");
+  }
+
+  PT_END(pt);
+}
+
+static int detect_thread(struct pt *pt)
+{
+  PT_BEGIN(pt);
+
+  while (1) {
     PT_YIELD_INTERVAL(1000);
-    printf("thread1\n");
+    printf("detect thread: something plugged!\n");
+    PT_SEM_SIGNAL(pt, &detect_evt);
+  }
+
   PT_END(pt);
 }
-
-static int protothread2(struct pt *pt)
-{
-  PT_BEGIN(pt);
-
-    PT_YIELD_usec(500);
-    printf("look I am thread2\n");
-  PT_END(pt);
-}
-
-static struct pt pt1, pt2;
 
 int main(void)
 {
   PT_INIT(&pt1);
   PT_INIT(&pt2);
+  PT_SEM_INIT(&detect_evt, 10);
 
   while(1) {
-    protothread1(&pt1);
-    protothread2(&pt2);
+    PT_SCHEDULE(main_thread(&pt1));
+    PT_SCHEDULE(detect_thread(&pt2));
   }
 }
